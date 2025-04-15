a022f4e03246fff746d5f2a6fe93840b1d77197d
Bogatu Alexei <alexeibogatu@gmail.com>
2025-04-15 09:50:24 +0300

    update

---
+++ JavaScript/index.js
@@ -0,0 +1,43 @@
+console.log("Salut, lume");
+
+function book(i, n, a, p){
+    this.img = i;
+    this.name = n;
+    this.author = a;
+    this.price = p;
+}
+
+function add(book){
+    const art = document.getElementById('art');
+    const div = document.createElement('div');
+    div.className = 'book';
+
+    let img = document.createElement('img');
+    img.src = book.img;
+    div.append(img);
+
+    let name = document.createElement('p');
+    name.className = 'name';
+    name.textContent = book.name;
+    div.append(name);
+
+    let author = document.createElement('p');
+    author.className = 'author';
+    author.textContent = book.author;
+    div.append(author);
+
+    let br = document.createElement('br');
+    div.append(br);
+
+    let price = document.createElement('p');
+    price.className = 'price';
+    price.textContent = book.price;
+    div.append(price);
+
+    art.append(div);
+}
+const nouaPatru = new book("C:/Users/Admin/Desktop/Practica/BookFan/img/1984.png", "1984", "George Orwell", "131 DML")
+add(nouaPatru);
+const At = new book(
+    "C:/Users/Admin/Desktop/Practica/BookFan/img/Atomic_Habits.png", "Atomic Habits", "James Clear", "254 DML");
+add (At);
